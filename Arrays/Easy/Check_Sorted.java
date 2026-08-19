fe440a49ed6623a5b9da4dfe1803e8416869df61
Rihashirin R <rihashirin0202@gmail.com>
2026-08-20 00:23:58 +0530

    Add Check_Sorted.java to verify array sorting

---
+++ Arrays/Easy/Check_Sorted.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Check_Sorted
+{
+    static boolean isSorted(int[] arr,int n)
+    {
+        for (int i=1;i<n;i++)
+        {
+            if(arr[i]>=arr[i-1]){
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(isSorted(arr,n)?"Sorted":"Unsorted");
+    }
+}
